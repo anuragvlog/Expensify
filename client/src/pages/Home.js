8f4bbdb822c4531bccc5791d0179103f3fdc990a
Code With Anurag <anurag.am2003@gmail.com>
2023-04-02 01:28:44 +0530

    add transaction ui added

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,70 @@
 import React from "react";
 import Layout from "../components/Layout";
+import { Form, Input, Modal, Select, } from "antd";
 
 const Home = () => {
   return (
     <Layout>
-      <h1>Home Page</h1>
+      <div className="filters">
+        <div>
+          <button
+            className="btn btn-primary"
+            onClick={() => setShowModal(true)}
+          >
+            Add New
+          </button>
+        </div>
+      </div>
+      <div className="content"></div>
+      <Modal
+        open={showModal}
+        onCancel={() => setShowModal(false)}
+        footer={false}
+      >
+        <Form
+          layout="vertical"
+          onFinish={handleSubmit}
+          initialValues={editable}
+        >
+          <Form.Item label="Amount" name="amount">
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item label="type" name="type">
+            <Select>
+              <Select.Option value="income">Income</Select.Option>
+              <Select.Option value="expense">Expense</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Category" name="category">
+            <Select>
+              <Select.Option value="salary">Salary</Select.Option>
+              <Select.Option value="tip">Tip</Select.Option>
+              <Select.Option value="project">Project</Select.Option>
+              <Select.Option value="food">Food</Select.Option>
+              <Select.Option value="movie">Movie</Select.Option>
+              <Select.Option value="bills">Bills</Select.Option>
+              <Select.Option value="medical">Medical</Select.Option>
+              <Select.Option value="fee">Fee</Select.Option>
+              <Select.Option value="tax">TAX</Select.Option>
+            </Select>
+          </Form.Item>
+          <Form.Item label="Date" name="date">
+            <Input type="date" />
+          </Form.Item>
+          <Form.Item label="Refrence" name="refrence">
+            <Input type="text" />
+          </Form.Item>
+          <Form.Item label="Description" name="description">
+            <Input type="text" />
+          </Form.Item>
+          <div className="d-flex justify-content-end">
+            <button type="submit" className="btn btn-primary">
+              {" "}
+              SAVE
+            </button>
+          </div>
+        </Form>
+      </Modal>
     </Layout>
   );
 };
